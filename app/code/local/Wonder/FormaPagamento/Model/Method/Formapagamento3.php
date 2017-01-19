8ef670ec5759daff459e1055165686ee2b250232
Rodrigo Rosa <rodrigo@wonder.com.br>
2017-01-19 10:02:37 -0200

    Adicionadas mais formas de pagamento

---
+++ app/code/local/Wonder/FormaPagamento/Model/Method/Formapagamento3.php
@@ -0,0 +1,6 @@
+<?php
+
+class Wonder_FormaPagamento_Model_Method_Formapagamento3 extends Mage_Payment_Model_Method_Abstract
+{
+  protected $_code = 'formapagamento3';
+}
---
+++ app/code/local/Wonder/FormaPagamento/Model/Method/Formapagamento5.php
@@ -0,0 +1,6 @@
+<?php
+
+class Wonder_FormaPagamento_Model_Method_Formapagamento5 extends Mage_Payment_Model_Method_Abstract
+{
+  protected $_code = 'formapagamento5';
+}
